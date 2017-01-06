84733219f51aa31f24d07a6357edcc6b9234e1e4
Ted Coyle <escvector@gmail.com>
2017-01-06 16:13:03 -0500

    fixed stupid script syntax typo issues

--- 09startcm.sh
+++ 09startcm.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
+   echo "USING REMOVE DATABASE"
    /usr/share/cmf/schema/scm_prepare_database.sh --host db mysql cm cm cm
 else
+   echo "USING LOCAL DATABASE"
    /usr/share/cmf/schema/scm_prepare_database.sh mysql cm cm cm
 fi 
 
--- runall.sh
+++ runall.sh
@@ -2,17 +2,18 @@
 
 # installs Cloudera Manager
 
-./03installcmrepo.sh
-./04installjdk.sh
-./05installjdbc.sh
+./03installcmrepo.sh $1
+./04installjdk.sh $1
+./05installjdbc.sh $1
 
 if [ $# -eq 0 ]; then
-   echo "USING REMOVE MYSQL DATABASE ON db.demo.cask.co"
+   echo "Using Remote Database"
 else
-	./06installlocalmysql.sh
+   ./06installlocalmysql.sh $1
+   ./07createdbusers.sh $1
 fi
 
-./08installcm.sh
-./09startcm.sh
+./08installcm.sh $1
+./09startcm.sh $1
 
 # python cm api cluster install
