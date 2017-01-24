6b06ae449611641230cb23ac4ac7604474d76770
Ted Coyle <escvector@gmail.com>
2017-01-23 22:35:09 -0500

    added cdapdl.ps1 and cdap-versions.txt

---
+++ cdapdl.ps1
@@ -0,0 +1,27 @@
+
+$global:i=0
+
+<#
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile($url,$localfile)
+#>
+
+# wget "http://www.contoso.com" -outfile "file"
+
+$version_list = import-csv .\cdap-versions.txt
+$selections = $version_list | Select @{Name="select";Expression={$global:i++;$global:i}},version,url
+write-output $selections | Select select,version | format-table
+
+[int]$selected = Read-Host 'Select Version to Download'
+
+$download = $selections | Where-Object {$_.select -eq $selected}
+
+$download | format-table |out-string | % {write-host $_}
+
+$localfile = $("cdap-sdk-"+$download.version+".zip")
+wget $download.url -outfile $localfile
+
+
+
+
+
